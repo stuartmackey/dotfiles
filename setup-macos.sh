eb344df6a0b540200f8e58772f7b77fdaffd4f6f
Stuart Mackey <stu.mackey@gmail.com>
2022-03-26 23:11:08 +0000

    Create setup-macos.sh

---
+++ setup-macos.sh
@@ -0,0 +1,10 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install brave-browser
+brew install kitty
+brew install gh
+brew install asdf 
+brew install nvim
+brew install fzf
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
