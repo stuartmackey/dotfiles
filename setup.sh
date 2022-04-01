3c8d255a068ba677856391a7c878091c8198fdd1
Stuart Mackey <stu.mackey@gmail.com>
2022-04-01 10:10:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo pacman -Syy
+sudo pacman -S --needed base-devel
+
+NONINTERACTIVE=1 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+#NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
 #brew install stow
