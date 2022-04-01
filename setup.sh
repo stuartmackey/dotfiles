e41a6ed438a9a0fd9990ee991d7359235c72f6e5
Stuart Mackey <stu.mackey@gmail.com>
2022-04-01 11:38:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 sudo pacman -Syy
-sudo pacman -S --needed base-devel
+sudo pacman -S --noconfirm --needed base-devel
 
-sudo pacman -S xf86-video-fbdev xorg xorg-xinit nitrogen picom
-sudo pacman -S stow fzf kitty
+sudo pacman -S --noconfirm xf86-video-fbdev xorg xorg-xinit nitrogen picom
+sudo pacman -S --noconfirm stow fzf kitty
 
-sudo pacman -S xorg-server xorg-apps
-sudo pacman -S i3-gaps i3blocks i3lock numlockx
+sudo pacman -S --noconfirm xorg-server xorg-apps
+sudo pacman -S --noconfirm i3-gaps i3blocks i3lock numlockx
 
-sudo pacman -S noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont
-sudo pacman -S ttf-liberation ttf-droid ttf-roboto terminus-font
+sudo pacman -S --noconfirm noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont
+sudo pacman -S --noconfirm ttf-liberation ttf-droid ttf-roboto terminus-font
 
-sudo pacman -S rxvt-unicode ranger rofi dmenu --needed
+sudo pacman -S --noconfirm rxvt-unicode ranger rofi dmenu --needed
 
 cd
 git clone https://aur.archlinux.org/yay.git
