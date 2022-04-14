967c984374399a25fb671eb39f663a5b70043f11
Stuart Mackey <stu.mackey@gmail.com>
2022-04-14 09:00:01 +0100

    Added new dependancies to i3 for polybar and moved some from the first set up into the i3 specific one

--- setup-i3.sh
+++ setup-i3.sh
@@ -1,1 +1,12 @@
-sudo pacman -S --noconfirm i3status
+sudo pacman -S --noconfirm i3-gaps i3blocks i3lock i3status
+
+sudo pacman -S --noconfirm noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont
+sudo pacman -S --noconfirm ttf-liberation ttf-droid ttf-roboto terminus-font
+
+sudo pacman -S --noconfirm rxvt-unicode ranger rofi dmenu --needed
+
+yay -S polybar
+
+yay -S nerd-fonts-jetbrains-mono ttf-font-awesome ttf-unifont nerd-fonts-hack nerd-fonts-victor-mono github-cli
+
+sudo pacman -S --noconfirm playerctl pavucontrol
--- setup.sh
+++ setup.sh
@@ -5,12 +5,7 @@ sudo pacman -S --noconfirm xf86-video-fbdev xorg xorg-xinit nitrogen picom
 sudo pacman -S --noconfirm stow fzf kitty
 
 sudo pacman -S --noconfirm xorg-server xorg-apps
-sudo pacman -S --noconfirm i3-gaps i3blocks i3lock numlockx
-
-sudo pacman -S --noconfirm noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont
-sudo pacman -S --noconfirm ttf-liberation ttf-droid ttf-roboto terminus-font
-
-sudo pacman -S --noconfirm rxvt-unicode ranger rofi dmenu --needed
+sudo pacman -S --noconfirm numlockx
 
 sudo pacman -S --noconfirm neovim tmux virtualbox openssh
 
