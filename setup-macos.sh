c3e7dbfe10956d6641838817d2ce7f24b55f8786
Stuart Mackey <stu.mackey@gmail.com>
2024-12-05 11:57:52 +0000

    Update setup-macos.sh

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,6 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install brave-browser
 brew install kitty
 brew install gh
 brew install asdf 
@@ -8,10 +7,7 @@ brew install nvim
 brew install fzf
 brew install alfred
 brew install 1password
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-brew install zoom
+brew install ripgrep
 brew install tableplus
 brew install stow
 
@@ -23,3 +19,5 @@ brew install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 brew install virtualbox
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
