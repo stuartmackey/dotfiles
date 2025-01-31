51c4be708f6e082907ace745fe17ae1cf3837ba8
Stuart Mackey <stu.mackey@gmail.com>
2025-01-31 07:50:32 +0000

    Split out fonts set up

--- setup-arch.sh
+++ setup-arch.sh
@@ -6,55 +6,18 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-echo "Kitty, Neovim, gh, stow, tmux, fzf, ripgrep, ripgrep, btop, Chrome, lazygit, sddm  ---------------------------------------------------------------------------"
-paru -y kitty neovim gh stow tmux fzf btop ripgrep google-chrome-stable lazygit sddm polybar rofi
+echo "Base utils ---------------------------------------------------------------------------"
+paru -y kitty neovim gh stow tmux fzf btop ripgrep google-chrome lazygit sddm polybar rofi unzip flameshot unzip lazydocker
 sudo update-alternatives --config x-terminal-emulator
 
 echo "Nerd Fonts --------------------------------------------------------------"
-declare -a fonts=(
-    BitstreamVeraSansMono
-    CodeNewRoman
-    DroidSansMono
-    FiraCode
-    FiraMono
-    Go-Mono
-    Hack
-    Hermit
-    JetBrainsMono
-    Meslo
-    Noto
-    Overpass
-    ProggyClean
-    RobotoMono
-    SourceCodePro
-    SpaceMono
-    Ubuntu
-    UbuntuMono
-    VictorMono
-)
-
-version='2.1.0'
-fonts_dir="${HOME}/.local/share/fonts"
-
-if [[ ! -d "$fonts_dir" ]]; then
-    mkdir -p "$fonts_dir"
-fi
-
-for font in "${fonts[@]}"; do
-    zip_file="${font}.zip"
-    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
-    echo "Downloading $download_url"
-    wget "$download_url"
-    unzip "$zip_file" -d "$fonts_dir"
-    rm "$zip_file"
-done
-
-find "$fonts_dir" -name '*Windows Compatible*' -delete
-
-fc-cache -fv
+cd ./dotfiles
+./setup-fonts.sh
 
 echo "asdf ------------------------------------------------------------------"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+cd ./dotfiles
+./setup-asdf.sh
 
 echo "aws -------------------------------------------------------------------"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -64,13 +27,15 @@ rm -rf ./aws
 rm awscliv2.zip
 
 echo "sddm --------------------------------------------------------"
-
-
+cd ./dotfiles/sddm
+sudo cp ./sddm.conf /etc/
+sudo unzip ./cattppuccin-mocha.zip -d /usr/share/sddm/themes
 
 echo "Working Folder --------------------------------------------------------"
 mkdir ~/Working
 mkdir ~/Working/GoReport
 mkdir ~/Working/personal
 
+echo "Set up git config -----------------------------------------------------"
 git config --global user.email "stu.mackey@gmail.com"
 git config --global user.name "Stuart Mackey"
